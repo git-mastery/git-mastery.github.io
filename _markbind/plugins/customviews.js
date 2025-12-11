ee6a776cf46c016abfff96a7cc5acd5338da155a
damithc <1673303+damithc@users.noreply.github.com>
2025-12-12 00:41:52 +0800

    Add one tab group

---
+++ _markbind/plugins/customviews.js
@@ -0,0 +1,7 @@
+function getScripts() {
+    return [
+        '<script src="https://unpkg.com/@customviews-js/customviews" data-base-url="/"></script>'
+    ];
+};
+
+module.exports = {getScripts};
